5d56e3442c112567a4cdc6061c6d65994d6fba37
alvaro <alvaromarflo@gmail.com>
2024-11-19 11:41:22 +0100

    ejercicio 1

--- Tema12/ejercicios/personas.py
+++ Tema12/ejercicios/personas.py
@@ -3,20 +3,17 @@ personas = fichero.read()
 personas = personas.split() # asi para que no me salgan con los \n
 fichero.close()
 
-dicc = {"id":"miau", "nombre":"miau", "apellido":"miau", "nacimiento":"miau"}
+listaDicc = []
 
 for i in personas:
     tupla = i.split(";")
-    print(tupla)
-    for j in tupla:
-        # meter en el dicc
-        print(j)
-        dicc
-        
-
-print(personas)
-
-
-#añadir a la lista personas
-
-# mostrar personas
+    # Crear un diccionario para cada persona
+    diccionario_persona = {
+        'id': tupla[0],
+        'nombre': tupla[1],
+        'apellido': tupla[2],
+        'nacimiento': tupla[3]
+    }
+    
+    # añadir a la lista personas
+    listaDicc.append(diccionario_persona)
