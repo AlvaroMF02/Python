7f56cf1298fb5b5a82f3994bb62cfac34d6d4de6
alvaro <alvaromarflo@gmail.com>
2024-11-21 12:58:23 +0100

    miau

--- Tema12/ejercicios/ejer2Personas/productos.py
+++ Tema12/ejercicios/ejer2Personas/productos.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 def verProductos():
     with open("productos.csv", newline="\n",encoding="utf8") as csvfile:
@@ -37,6 +38,61 @@ def borrar(idBorr):
                 id, nombre, categoria, precio, stock, imagen
             })
 
+def filtrar(categ):
+    prod = []
+    # creo una lista con los productos en tuplas
+    with open("productos.csv", newline="\n",encoding="utf8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for id, nombre, categoria, precio, stock,imagen in reader:
+            p = (id, nombre, categoria, precio, stock, imagen)
+            prod.append(p)
+    
+    for i in prod:
+        if categ == i[2].lower():
+            print(i)
+            
+def buscar(nom):
+    prod = []
+    # creo una lista con los productos en tuplas
+    with open("productos.csv", newline="\n",encoding="utf8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for id, nombre, categoria, precio, stock,imagen in reader:
+            p = (id, nombre, categoria, precio, stock, imagen)
+            prod.append(p)
+    
+    for i in prod:
+        if nom == i[1].lower():
+            print(i)
+            
+def exportarJson():
+    prod = []
+    with open("productos.csv", newline="\n",encoding="utf8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for id, nombre, categoria, precio, stock, imagen in reader:
+            p = (id, nombre, categoria, precio, stock, imagen)
+            prod.append(p)
+
+    datos = []
+    for id, nombre, categoria, precio, stock, imagen in prod:
+        datos.append({"id":id, "nombre":nombre, "categoria":categoria, "precio":precio, "stock":stock, "imagen":imagen})
+
+    with open("productos.json", "a+") as jsonfile:
+        json.dump(datos, jsonfile)
+    
+    
+            
+def exportarXml():
+    prod = []
+    with open("productos.csv", newline="\n",encoding="utf8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for id, nombre, categoria, precio, stock,imagen in reader:
+            p = (id, nombre, categoria, precio, stock, imagen)
+            prod.append(p)
+    
+    
+
+
+
 resp = 0
 
 while resp != "5":
@@ -45,7 +101,10 @@ while resp != "5":
     print("2) Ver todos los producto")
     print("3) Eliminar un producto")
     print("4) Actualizar un producto")
-    print("5) Salir")
+    print("5) Filtrar por categoría")
+    print("6) Buscar producto")
+    print("7) Exportar productos")
+    print("8) Salir")
     print("*********************************")
     
     resp = input("¿Qué desea hacer? ")
@@ -64,9 +123,36 @@ while resp != "5":
         verProductos()
         
     elif resp == "3":
-        id = input("ID: ")
-        borrar(id)
+        print("Sin hacer")
+        # id = input("ID: ")
+        # borrar(id)
     elif resp == "4":
         print("Lo actualiza")
+    elif resp == "5":
+        print("----------------------")
+        print("- Electrónica")
+        print("- Deportes")
+        print("- Hogar")
+        print("- Juguetes")
+        print("- Ropa")
+        print("----------------------")
+        categ = input("¿Por qué desea filtrar? (debe escribir el nombre)")
+        filtrar(categ.lower())
+    elif resp == "6":
+        categ = input("¿Qué está buscando?")
+        buscar(categ.lower())
+    elif resp == "7":
+        print("----------------------")
+        print("1) JSON")
+        print("2) XML")
+        print("3) Cancelar")
+        print("----------------------")
+        expor = input("¿En qué desea exportar?")
+        if expor == "1":
+            exportarJson()
+        elif expor == "2":
+            pass
+        else:
+            print("Cancelado")
     else:
         print("Chao")
